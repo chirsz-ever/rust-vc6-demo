56725346b6398061387e8f8effa4719b9b5b6e77
chirsz-ever <chirsz@foxmail.com>
2023-04-11 06:37:08 +0800

    init commit: successfully run Hello World

---
+++ src/stub.c
@@ -0,0 +1,17 @@
+int __cdecl wcsicmp() {
+
+}
+
+int __stdcall SHSetFolderPathW(int _0, int _1, int _2, int _3){
+
+}
+
+int __stdcall _imp__GetUserProfileDirectoryW(int _0, int _1, int _2) {
+
+}
+
+extern void abort();
+
+int __cdecl __CxxFrameHandler3() {
+    abort();
+}
