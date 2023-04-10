ec29f104faf89cba0d82b3a156845ad92e2d4105
chirsz-ever <chirsz@foxmail.com>
2023-04-11 07:50:31 +0800

    impove: support guessing number

--- link_wrapper.py
+++ link_wrapper.py
@@ -7,9 +7,17 @@ unused_libs = ["userenv.lib", "legacy_stdio_definitions.lib", "bcrypt.lib"]
 
 args = []
 for x in sys.argv:
-    if x in unused_libs:
-        continue
-    args.append(x)
+    if x.startswith('@'):
+        with open(x[1:], encoding='utf16') as f:
+            for a in f:
+                a = a[1:-2]
+                if a in unused_libs:
+                    continue
+                args.append(a)
+    else:
+        if x in unused_libs:
+            continue
+        args.append(x)
 
 args[0] = "LINK.EXE"
 
