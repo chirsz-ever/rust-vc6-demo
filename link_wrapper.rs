79a6b842ddbb3ab56d677fe7d148c8509d467383
chirsz-ever <chirsz@foxmail.com>
2023-04-15 21:27:05 +0800

    update link_wrapper.rs

--- link_wrapper.rs
+++ link_wrapper.rs
@@ -1,8 +1,20 @@
 use std::io::{self, prelude::*};
+use std::collections::BTreeMap;
 
-const UNSUPPORTED_LIBS: &[&str] = &["legacy_stdio_definitions.lib", "bcrypt.lib"];
+fn add_arg(arg_map: &BTreeMap<&str, Option<&str>>, args: &mut Vec<String>, a: &str) {
+    match arg_map.get(a) {
+        None => args.push(a.to_string()),
+        Some(Some(a)) => args.push(a.to_string()),
+        Some(None) => {},
+    }
+}
 
 fn main() -> std::process::ExitCode {
+    let args_map = BTreeMap::from([
+        ("legacy_stdio_definitions.lib", None),
+        ("bcrypt.lib", None),
+        ("ntdll.lib", None),
+    ]);
     let mut new_args = Vec::new();
     for arg in std::env::args() {
         if let Some(response_file) = arg.strip_prefix('@') {
@@ -11,21 +23,31 @@ fn main() -> std::process::ExitCode {
             let response_file_content = String::from_utf16(&response_file_utf16).unwrap();
             for a in response_file_content.split_ascii_whitespace() {
                 let a = &a[1..a.len() - 1];
-                if !UNSUPPORTED_LIBS.contains(&a) {
-                    new_args.push(a.to_owned());
-                }
+                add_arg(&args_map, &mut new_args, a);
             }
-        } else if !UNSUPPORTED_LIBS.contains(&arg.as_str()) {
-            new_args.push(arg.to_owned());
+        } else {
+            add_arg(&args_map, &mut new_args, &arg);
         }
     }
     let output = std::process::Command::new("LINK.EXE")
         .args(&new_args[1..])
         .output()
         .unwrap();
-    std::io::stdout().write_all(&output.stdout).unwrap();
-    std::io::stderr().write_all(&output.stderr).unwrap();
+    std::io::stdout().write_all(&escape(&output.stdout)).unwrap();
+    std::io::stderr().write_all(&escape(&output.stderr)).unwrap();
     std::process::ExitCode::from(output.status.code().unwrap_or(1) as u8)
+}
+
+fn escape(data: &[u8]) -> Vec<u8> {
+    let mut result = Vec::new();
+    for b in data {
+        if b.is_ascii_graphic() || b.is_ascii_whitespace() {
+            result.push(*b);
+        } else {
+            result.extend(b.escape_ascii());
+        }
+    }
+    result
 }
 
 fn read_utf16_file(path: &str) -> io::Result<Vec<u16>> {
