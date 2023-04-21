72bac21b1a73bf76da11f78b3d5c87dc2f0cfb18
chirsz-ever <chirsz@foxmail.com>
2023-04-22 01:39:03 +0800

    panic when subprocess fail in build script

--- build.rs
+++ build.rs
@@ -6,19 +6,21 @@ fn main() {
     if target == "i686-pc-windows-msvc" {
         let def_file = "shell32_stub.def";
         let out_dir = env::var("OUT_DIR").unwrap();
-        Command::new("lib")
+        let status = Command::new("lib")
             .args(["/NOLOGO", "/MACHINE:IX86"])
             .arg(format!("/DEF:{def_file}"))
             .arg(format!("/OUT:{out_dir}\\shell32_stub.lib"))
             .status()
             .unwrap();
+        assert_eq!(status.code(), Some(0));
 
         let link_wrapper_src = "link_wrapper\\link_wrapper.rs";
         let link_wrapper_exe = "link_wrapper\\link_wrapper.exe";
-        Command::new("rustc")
+        let status = Command::new("rustc")
             .args([link_wrapper_src, "-o", link_wrapper_exe])
             .status()
             .unwrap();
+        assert_eq!(status.code(), Some(0));
 
         println!("cargo:rustc-link-lib=shell32_stub");
         println!("cargo:rustc-link-search={out_dir}");
